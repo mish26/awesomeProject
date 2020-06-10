adfd0a4b091ad5b05ba2b4af51c7a8ca93b62e7f
mishrun <mishrun.git@gmail.com>
2020-06-10 22:40:38 +0900

    update talib

--- thirdPartyPackage/talib.go
+++ thirdPartyPackage/talib.go
@@ -9,11 +9,14 @@ import (
 func main(){
 	spy, _ := quote.NewQuoteFromYahoo(
 		"spy",
-		"2018-04-01",
-		"2019-01-01",
+		"2020-04-01",
+		"2020-06-10",
 		quote.Daily,
 		true)
 	fmt.Print(spy.CSV())
 	rsi2 := talib.Rsi(spy.Close, 2)
 	fmt.Println(rsi2)
+
+	mva := talib.Ema(spy.Close,14)
+	fmt.Println(mva)
 }
