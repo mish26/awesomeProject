5213d018360393eaeadb14d99a5a39f48229bc66
mishrun <mishrun.git@gmail.com>
2020-06-13 23:43:36 +0900

    use cache

--- WritingWebApplications/app.go
+++ WritingWebApplications/app.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Page struct {
@@ -14,6 +15,8 @@ type Page struct {
 
 const runtimePath string = "./WritingWebApplications/"
 
+// cache
+var templates = template.Must(template.ParseFiles(runtimePath + "edit.html", runtimePath + "view.html"))
 
 func (p *Page) save() error {
 	filename := runtimePath + p.Title + ".txt"
@@ -31,12 +34,16 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(runtimePath + tmpl + ".html")
-	t.Execute(w, p)
+	//t, _ := template.ParseFiles(runtimePath + tmpl + ".html")
+	//t.Execute(w, p)
+	err := templates.ExecuteTemplate(w,  tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request)  {
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string)  {
+//	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
@@ -45,8 +52,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request)  {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request)  {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string)  {
+//	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -55,8 +62,8 @@ func editHandler(w http.ResponseWriter, r *http.Request)  {
 
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+//	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title:title, Body: []byte(body)}
 	err := p.save()
@@ -67,10 +74,24 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+
+	return func(writer http.ResponseWriter, request *http.Request) {
+		m := validPath.FindStringSubmatch(request.URL.Path)
+		if m == nil {
+			http.NotFound(writer, request)
+			return
+		}
+		fn(writer, request, m[2])
+	}
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 func main(){
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
